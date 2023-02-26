57eb3a4a03d8205bf8068ed6f750d6419b277875
Page Carbajal <awesome@pagecarbajal.com>
2023-02-25 23:23:24 -0600

    Astro Image

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
-    integrations: [
-        tailwind({
-            config: {
-            },
-          }),
-    ],
-    tailwindConfig: './tailwind.config.js',
+  integrations: [tailwind({
+    config: {}
+  }), image()],
+  tailwindConfig: './tailwind.config.js'
 });
-
-
