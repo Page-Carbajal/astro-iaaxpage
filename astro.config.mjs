7eee4529457909d081c54d0451880cc09b36b3a2
Page Carbajal <awesome@pagecarbajal.com>
2023-02-26 18:09:52 -0600

    Sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,27 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const homeUrl = process.env.HOME_URL;
+
 
 // https://astro.build/config
 import image from "@astrojs/image";
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
+  site: homeUrl,
   integrations: [tailwind({
     config: {}
-  }), image()],
+  }), image(), sitemap({
+    changefreq: 'weekly',
+    priority: 0.7,
+    lastmod: new Date('2022-02-24'),
+  })],
   tailwindConfig: './tailwind.config.js'
 });
