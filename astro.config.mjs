c6482f529b878b9fe2ddb4ffbdf22be3939a113c
Page Carbajal <awesome@pagecarbajal.com>
2023-02-26 18:37:30 -0600

    - Added support for partytown - Added support for sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import dotenv from "dotenv";
-
 dotenv.config();
-
 const homeUrl = process.env.HOME_URL;
-
 
 // https://astro.build/config
 import image from "@astrojs/image";
@@ -14,14 +11,26 @@ import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
 export default defineConfig({
   site: homeUrl,
   integrations: [tailwind({
     config: {}
   }), image(), sitemap({
-    changefreq: 'weekly',
-    priority: 0.7,
-    lastmod: new Date('2022-02-24'),
+    i18n: {
+      defaultLocale: 'es'
+    },
+    serialize(page) {
+      console.log('Page: ', page);
+      
+      return page;
+    }
+  }), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
   })],
   tailwindConfig: './tailwind.config.js'
 });
